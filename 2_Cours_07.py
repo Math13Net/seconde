c0a31c1e827a699a94e08c00f7093b62db74723d
Christie Vassilian <36983385+Math13Net@users.noreply.github.com>
2018-03-04 14:12:07 +0100

    Update 2_Cours_07.py

--- 2_Cours_07.py
+++ 2_Cours_07.py
@@ -1,1 +1,20 @@
-test
+# 2_Cours_07
+# Parabole
+
+def carre(a):
+  X=[]
+  Y=[]
+  for i in range(a,5):
+    X.append(i)
+    Y.append(i**2)
+  print(X)
+  print(Y)
+  
+ def carre_bis(a,b):
+  X=[]
+  Y=[]
+  for i in range(a,b+1):
+    X.append(i)
+    Y.append(i**2)
+  print(X)
+  print(Y)
