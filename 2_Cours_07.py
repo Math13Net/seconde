a96f5ce7b59d263dcc8be63e8e144013757cbb1d
Christie Vassilian <36983385+Math13Net@users.noreply.github.com>
2018-03-04 18:10:43 +0100

    Update 2_Cours_07.py

--- 2_Cours_07.py
+++ 2_Cours_07.py
@@ -24,3 +24,29 @@ def carre(a):
   while V**2 < M:
     V = V + 1
   print(V)
+
+ def f(x):
+  return a*x**2+b*x+c
+
+def sommet_parabole():
+  S=-100
+  if a>0:
+    for i in range(-100,101):
+      if f(i)<f(S):
+        S=i
+    print("le sommet est à proximité du point:")     
+    print("Sx = ",S)
+    print("Sy = ",f(S))
+  else:
+    for i in range(-100,101):
+      if f(i) > f(S):
+        S=i
+    print("le sommet est à proximité du point:")    
+    print("Sx = ",S)
+    print("Sy = ",f(S))
+    
+a = eval(input(" a = "))
+b = eval(input(" b = "))
+c = eval(input(" c = "))
+sommet_parabole()
+
