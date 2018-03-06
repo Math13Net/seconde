ded655c4fa9bc507021ae41fb8b9520fa14a2b2b
Christie Vassilian <36983385+Math13Net@users.noreply.github.com>
2018-03-06 17:52:48 +0100

    Update 2_Cours_06.py

--- 2_Cours_06.py
+++ 2_Cours_06.py
@@ -1,6 +1,7 @@
 # 2_Cours_06
 # Probabilités
 
+-----------------------------------------------------------------------------------------
 import random as rd
 
 def approx_pi(n):
@@ -11,11 +12,26 @@ def approx_pi(n):
   compteur = 0
   
   for i in range(n):
-	  X = rd.random()
-	  Y = rd.random()
-	  if X[i]**2 + Y[i]**2 < 1:
-	    compteur += 1
+		X = rd.random()
+		Y = rd.random()
+		if X[i]**2 + Y[i]**2 < 1:
+	    		compteur += 1
 
   print("Prob = ", round(compteur*4/n, 5))
 
+---------------------------------------------------------------------------------------------
 
+from math import sqrt
+import random as rd
+
+def approx_pi_2(n):
+	compteur = 0
+
+  for i in range(n):
+	  X = rd.random()
+	  Y = sqrt(1-X**2)
+	  Z = rd.random()
+	  if Z < Y:
+	    compteur += 1
+  
+	print("Prob = ", round(compteur*4/n, 5))
