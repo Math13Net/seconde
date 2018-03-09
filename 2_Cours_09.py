23435d337a4e135d8f64095b823bcd7ea9a4418d
Christie Vassilian <36983385+Math13Net@users.noreply.github.com>
2018-03-09 14:06:56 +0100

    Update 2_Cours_09.py

--- 2_Cours_09.py
+++ 2_Cours_09.py
@@ -15,3 +15,22 @@ def droite_2_points(xa,ya,xb,yb):
     print("l'équation de la droite est : x = ",xa)
     
 ----------------------------------------------------------------------------------    
+from math import *
+
+def droite_2_points(xa,ya,xb,yb):
+  if xa != xb:
+    a = (yb-ya)/(xb-xa)
+    b = ya -a*xa
+    print("l'équation de la droite est : y = ",a,"x + ",b)
+  else:
+    print("l'équation de la droite est : x = ",xa)
+    
+def position_droite(a1,b1,a2,b2):
+  if a1==a2:
+    print("les 2 droites sont parallèles")
+  elif a1*a2==-1:
+    print("les 2 droites sont perpendiculaires")
+  else:
+    x = (b2-b1)/(a1-a2)
+    y = a1*(b2-b1)/(a1-a2)+b1
+    print("(",x,",",y,") est le point d'intersection des 2 droites")
